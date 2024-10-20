66fd0ad8434e812f14b241087d729bd2726c2bd2
Lior Silberman <silberman1015@gmail.com>
2024-10-20 03:24:01 +0300

    Add context for easy authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signup from './components/signup/Signup';
 import Login from './components/login/Login';
@@ -7,15 +7,26 @@ import AdminLogin from './components/adminLogin/AdminLogin';
 import AdminPage from './components/adminPage/AdminPage';
 import AdminResults from './components/adminResults/AdminResults';
 import Live from './components/live/Live';
+import Navbar from './components/navbar/Navbar';
+import AdminSignup from './components/adminSignup/AdminSignup';
+
+
 
 const App = () => {
+
+  useEffect(() => {
+    document.title = 'JaMoveo';
+  }, []);
+
   return (
     <Router>
       <div>
+        <Navbar />
         <Routes>
           <Route path='/signup' Component={Signup} />
           <Route path='/login' Component={Login} />
-          <Route path='/admin-login' Component={AdminLogin} />
+          <Route path='/admin/login' Component={AdminLogin} />
+          <Route path='/admin/signup' Component={AdminSignup} />
           <Route path='/admin' Component={AdminPage} />
           <Route path='/results' Component={AdminResults} />
           <Route path='/' Component={Home} />
